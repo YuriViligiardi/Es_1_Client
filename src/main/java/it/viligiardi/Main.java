6851a07834c710d0620046089ae742e69b53b727
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-09 08:52:53 +0200

    Creato mySocket e funziona il collegamento col server

--- src/main/java/it/viligiardi/Main.java
+++ src/main/java/it/viligiardi/Main.java
@@ -1,7 +1,15 @@
 package it.viligiardi;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("Il client è partito");
+
+        Socket mySocket = new Socket("localhost", 5637); //Creazione Socket del server che andrà in comunicazione con quello del server
+        
+        System.out.println("Il client si è collegato");
     }
 }
